6ac7a1872aa76fa5e2260e30c9a0496122df62f9
AntonioDG30 <a.digiorgio8@studenti.unisa.it>
2024-08-17 13:44:37 +0200

    Primi bugfix e studio funzionamento Flash

--- app.py
+++ app.py
@@ -2,9 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Definisci la route per la homepage
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 if __name__ == '__main__':
+    # Avvia l'app in modalità debug
     app.run(debug=True)
